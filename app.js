ca305a001ebee98dc53d984917a09a45d07cd54a
Punith7899 <112472715+Punith7899@users.noreply.github.com>
2022-08-30 14:00:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+})();
